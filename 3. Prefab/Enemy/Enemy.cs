b23d8fcc931dae6a79ba0a734738dc328eebad5a
yotoro <2b_myself@naver.com>
2014-06-11 16:59:54 +0900

    ㅋㅋㅋㅋ

--- 3. Prefab/Enemy/Enemy.cs
+++ 3. Prefab/Enemy/Enemy.cs
@@ -4,12 +4,26 @@ using System.Collections;
 public class Enemy : MonoBehaviour
 {
     public float speed = 10.0f;
-    public float rayDistance = 10.0f;
-
+    public float rayDistance = 1.0f;
 
     void Update()
     {
-        종상이바보
         gameObject.transform.Translate(new Vector3(0, 0, 1) * speed * Time.deltaTime);
+
+        detection();
+    }
+
+    void detection()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * rayDistance, Color.red);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance))
+        {
+            if (hit.collider.gameObject.name == "Bottom")
+            {
+                gameObject.SetActive(false);
+            }
+        }
     }
 }
